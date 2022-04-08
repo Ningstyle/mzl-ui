adef10503927c2d78cd99093fa661c4cf7dbcd97
张国喜 <2458734304@qq.com>
2022-04-08 12:29:54 +0800

    fix Message text过长导致超出message框的问题
    update Message text字段支持VNode

--- packages/message/index.js
+++ packages/message/index.js
@@ -1,23 +1,28 @@
 import { createVNode, render } from 'vue'
 import mMessage from './index.vue'
 
-export default ({ text, type,timeout,icon,textColor,bgColor,customClass}) => {
+export default ({ text, type, timeout, icon, textColor, bgColor, customClass }) => {
   // 动态创建一个DOM容器
-  const div = typeof document !== 'undefined'?typeof document.createElement !== 'undefined'?document.createElement('div'):'':''
+  const div =
+    typeof document !== 'undefined'
+      ? typeof document.createElement !== 'undefined'
+        ? document.createElement('div')
+        : ''
+      : ''
   div.setAttribute('class', 'mzlui-meassage-container')
-  if(typeof document !== 'undefined'){
+  if (typeof document !== 'undefined') {
     document.body.appendChild(div)
   }
   let timer = null
   // 传递给组件的选项
-  const vnode = createVNode(mMessage, { text, type,timeout,icon,textColor,bgColor,customClass})
+  const vnode = createVNode(mMessage, { text, type, timeout, icon, textColor, bgColor, customClass }, [text])
   render(vnode, div)
   clearTimeout(timer)
   timer = setTimeout(() => {
     render(null, div)
-    if(typeof document !== 'undefined'){
+    if (typeof document !== 'undefined') {
       document.body.removeChild(div)
     }
     clearTimeout(timer)
-  }, timeout||2500)
+  }, timeout || 2500)
 }
