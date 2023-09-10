448fd4bc9eceb07dac4c0736f829dde1ce2c0a65
lining <1046674804@qq.com>
2023-09-10 11:16:33 +0800

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,28 @@ export default defineConfig({
   build: {
     rollupOptions: {
       // 请确保外部化那些你的库中不需要的依赖
-      external: ["vue","highlight.js","jest","pinia","sass","sass-loader","vite-plugin-md","vue-router","@babel/preset-env","@vitejs/plugin-vue","@vue/reactivity","@vue/test-utils","babel-jest","babel-preset-vite","eslint","eslint-plugin-vue","vite","vue-jest","commitizen","cz-conventional-changelog"],
+      external: [
+        "vue",
+        "highlight.js",
+        "jest",
+        "pinia",
+        "sass",
+        "sass-loader",
+        "vite-plugin-md",
+        "vue-router",
+        "@babel/preset-env",
+        "@vitejs/plugin-vue",
+        "@vue/reactivity",
+        "@vue/test-utils",
+        "babel-jest",
+        "babel-preset-vite",
+        "eslint",
+        "eslint-plugin-vue",
+        "vite",
+        "vue-jest",
+        "commitizen",
+        "cz-conventional-changelog",
+      ],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
@@ -21,7 +42,7 @@ export default defineConfig({
       },
     },
     lib: {
-      entry: "./packages/index.js",
+      entry: "./packages/index.ts",
       name: "mzl-ui",
     },
   },
